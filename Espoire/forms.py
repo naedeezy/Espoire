e9e766eadb2b23e55d72e4871d17a6f8245ade6b
naedeezy <naedeezy@gmail.com>
2021-11-01 12:53:07 +0000

    Modification to the transaction form...nothing major

--- Espoire/forms.py
+++ Espoire/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -9,7 +10,17 @@ from .models import Transaction
 class TransactionForm(ModelForm):
 	class Meta:
 		model = Transaction
-		fields = "__all__"
+		fields = ('trans_type', 'number', 'amount', 'name', 'trans_id')
+
+
+
+		widgets = {
+			'trans_type': forms.TextInput(attrs={'class': "form-control"}),
+			'number': forms.NumberInput(attrs={'class': "form-control"}),
+			'amount': forms.TextInput(attrs={'class': "form-control"}),
+			'name': forms.TextInput(attrs={'class': "form-control"}),
+			'trans_id': forms.TextInput(attrs={'class': "form-control"}),
+		}
 
 class CreateUserForm(UserCreationForm):
 	class Meta:
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-z#khaucs0p^u83*tx+xn)2qcknw#f^@-1@pim!oq)mke-+0uv1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
